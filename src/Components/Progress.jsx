7762ba76fffce6811f0f985e5e171c605d5e7d41
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-12 23:02:10 -0400

    Add target value to progress.

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,4 +1,4 @@
-export default function Progress({ donations }) {
+export default function Progress({ donations, targetAmount }) {
   let sum = 0;
   for (let d of donations) {
     sum += d.amount;
@@ -7,7 +7,7 @@ export default function Progress({ donations }) {
     <section className="progress">
       <h2>
         Raised <span className="secondary">${sum}</span> of
-        <span className="secondary">$1000</span>
+        <span className="secondary">${targetAmount}</span>
       </h2>
     </section>
   );
