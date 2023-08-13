519a4c72a6014927b0a41f8c8c405325b184e741
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-12 23:02:38 -0400

    Complete recent donations component.

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,19 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ donations }) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map(({ name, amount, caption }) => {
+          return (
+            <li>
+              <span>
+                {name} donated ${amount}
+              </span>
+              {caption}
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
