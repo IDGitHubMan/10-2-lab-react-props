67d7da8113229c2d849b86cb59ce57faa9d88a1d
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-12 22:54:44 -0400

    Complete progress component.

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
+export default function Progress({ donations }) {
+  let sum = 0;
+  for (let d of donations) {
+    sum += d.amount;
+  }
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">${sum}</span> of
+        <span className="secondary">$1000</span>
+      </h2>
+    </section>
+  );
 }
